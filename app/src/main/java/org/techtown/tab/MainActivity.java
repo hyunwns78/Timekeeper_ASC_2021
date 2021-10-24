36e4c9460410d49d61757161fbe5432d932c0845
ALEX <alexization@gmail.com>
2021-10-24 23:49:21 +0900

    Change 1

--- app/src/main/java/org/techtown/tab/MainActivity.java
+++ app/src/main/java/org/techtown/tab/MainActivity.java
@@ -1,19 +1,21 @@
 package org.techtown.tab;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    androidx.appcompat.widget.Toolbar toolbar;
-    private TabLayout tabs;
     fragment_main fragment_main;
     fragment_activity fragment_activity;
     fragment_graph fragment_graph;
@@ -23,53 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(false);
-
         fragment_main = new fragment_main();
         fragment_activity = new fragment_activity();
         fragment_graph = new fragment_graph();
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_main).commit();
 
-        tabs = findViewById(R.id.tabs);
-        tabs.addTab(tabs.newTab().setText("MAIN"));
-        tabs.addTab(tabs.newTab().setText("ACTIVITY"));
-        tabs.addTab(tabs.newTab().setText("GRAPH"));
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                Log.d("MainActivity", "선택된 탭 : " + position);
-
-                Fragment selected = null;
-                if (position == 0){
-                    selected = fragment_main;
+                        switch (item.getItemId()){
+                            case R.id.tab_main:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_main).commit();
+                            case R.id.tab_activty:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_activity).commit();
+                                return true;
+                            case R.id.tab_graph:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment_graph).commit();
+                                return true;
+                        }
+                        return false;
+                    }
                 }
-                else if (position == 1){
-                    selected = fragment_activity;
-                }
-                else if (position == 2){
-                    selected = fragment_graph;
-                }
-
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        );
     }
 
 }
--- app/src/main/java/org/techtown/tab/fragment_main.java
+++ app/src/main/java/org/techtown/tab/fragment_main.java
@@ -1,25 +1,35 @@
 package org.techtown.tab;
 
+import android.content.ClipData;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 public class fragment_main extends Fragment {
 
     PieChart pieChart;
+    public RecyclerView recyclerView;
+    ArrayList<ClipData.Item> itemList;
 
     @Nullable
     @Override
@@ -32,21 +42,27 @@ public class fragment_main extends Fragment {
         return rootView;
     }
 
+    ArrayList insertItem(){
+        itemList = new ArrayList<>();
+        return itemList;
+    }
+
     private void initUI(ViewGroup rootView){
+
         pieChart = rootView.findViewById(R.id.piechart);
         pieChart.setUsePercentValues(true);
         pieChart.getDescription().setEnabled(false);
         pieChart.setCenterText("활동비율");
         pieChart.setTransparentCircleColor(Color.WHITE);
         pieChart.setTransparentCircleAlpha(110);
-        pieChart.setHoleRadius(58f);
-        pieChart.setTransparentCircleRadius(61f);
+        pieChart.setHoleRadius(38f);
+        pieChart.setTransparentCircleRadius(30f);
         pieChart.setDrawCenterText(true);
         pieChart.setHighlightPerTapEnabled(true);
         Legend legend1 = pieChart.getLegend();
         legend1.setEnabled(false);
-        pieChart.setEntryLabelColor(Color.WHITE);
-        pieChart.setEntryLabelTextSize(12f);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setEntryLabelTextSize(16f);
 
         setData1();
     }
@@ -54,15 +70,21 @@ public class fragment_main extends Fragment {
     private void setData1(){
         ArrayList<PieEntry> entries = new ArrayList<>();
 
-        entries.add(new PieEntry(20, "JAVA"));
-        entries.add(new PieEntry(40, "PYTHON"));
-        entries.add(new PieEntry(40, "C"));
+        entries.add(new PieEntry(20, "공부"));
+        entries.add(new PieEntry(40, "운동"));
+        entries.add(new PieEntry(40, "휴식"));
 
         PieDataSet dataSet = new PieDataSet(entries, "활동 비율");
 
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int c : ColorTemplate.JOYFUL_COLORS){
+            colors.add(c);
+        }
+        dataSet.setColors(colors);
+
         PieData data = new PieData(dataSet);
-        data.setValueTextSize(22f);
-        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(26f);
+        data.setValueTextColor(Color.BLACK);
 
         pieChart.setData(data);
         pieChart.invalidate();
